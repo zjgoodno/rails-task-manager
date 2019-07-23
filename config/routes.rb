1a04780dce19403ff22c8b5df344bb815b2a177d
Zachary Goodnough <goodnough.z@gmail.com>
2019-07-23 16:03:11 +0800

    task manager without working checkboxes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "tasks", to: "tasks#index"
+
+  get "tasks/new", to: "tasks#new", as: :new
+
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  post "tasks", to: "tasks#create", as: :create
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+
+  patch "tasks/:id", to: "tasks#update", as: :update
+
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy
+
+
+
 end
